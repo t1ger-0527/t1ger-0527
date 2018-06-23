8409d734d6cc1e57b6442a0904f919bfbe4305b7
t1ger@zhihu.com <t1ger@zhihu.com>
2018-06-23 16:53:34 +0800

    :sparkles: feat: first blog

---
+++ path---index-3acfe1d0056a3fcff7f1.js
@@ -0,0 +1,2 @@
+webpackJsonp([0x81b8806e4260],{399:function(e,t){e.exports={data:{site:{siteMetadata:{title:"T1ger Blog"}},allMarkdownRemark:{edges:[{node:{excerpt:"After two years of working as an software engineer, I finally decided to start my blog. A Dramatic Life Change The very first reason I made…",fields:{slug:"/hello-world/"},frontmatter:{date:"23 June, 2018",title:"Hello World"}}}]}},pathContext:{}}}});
+//# sourceMappingURL=path---index-3acfe1d0056a3fcff7f1.js.map
